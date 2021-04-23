764fa51f8d0b7a18a70d7e436ba30115610c7f5b
cybarspace <cybardev@pm.me>
2021-04-23 21:45:06 +0600

    update

--- picalc.py
+++ picalc.py
@@ -24,7 +24,8 @@ def pi(precision=42):
 
     # main calculation process
     while s != ed:
-        ed, n, na, d, da = s, n+na, n+8, d+da, d+32
+        (ed, _), (n, na), (d, da) = map(adder,
+                                         ((0, s, 0), (na, n, 8), (da, d, 32)))
         t = (t * n) / d
         s += t
 
